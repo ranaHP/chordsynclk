d45699aec00ca130faf4239b55fe1da2aa02ac8d
R_Hansana <hansana.ranaweera@taprotechnologies.com>
2026-06-21 19:01:54 +0300

    intial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [
-    tanstackStart({
-      server: { entry: "server" },
-    }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackRouter(), react(), tailwindcss()],
   server: {
     host: "127.0.0.1",
     port: 5173,
